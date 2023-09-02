552cf6206d60af4af5f41402c1cfef71f3bb9a1e
Fran <caviglia80@gmail.com>
2023-09-02 15:13:37 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from '@routes/login/login.component';
+import { RegisterComponent } from '@routes/register/register.component';
+import { LandingPageComponent } from '@routes/landing-page/landing-page.component';
+import { PaymentComponent } from '@routes/payment/payment.component';
+import { DashboardComponent } from '@routes/dashboard/dashboard.component';
+import { IngresosComponent } from '@routes/ingresos/ingresos.component';
+import { EgresosComponent } from '@routes/egresos/egresos.component';
+import { InventoryComponent } from '@routes/inventory/inventory.component';
+import { ReportsComponent } from '@routes/reports/reports.component';
+import { BillingComponent } from '@routes/billing/billing.component';
+import { ConfigurationComponent } from '@routes/configuration/configuration.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'landingPage', component: LandingPageComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'ingresos', component: IngresosComponent },
+  { path: 'egresos', component: EgresosComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'billing', component: BillingComponent },
+  { path: 'configuration', component: ConfigurationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+import { LoginComponent } from '@routes/login/login.component';
+import { RegisterComponent } from '@routes/register/register.component';
+import { LandingPageComponent } from '@routes/landing-page/landing-page.component';
+import { PaymentComponent } from '@routes/payment/payment.component';
+import { DashboardComponent } from '@routes/dashboard/dashboard.component';
+import { IngresosComponent } from '@routes/ingresos/ingresos.component';
+import { EgresosComponent } from '@routes/egresos/egresos.component';
+import { InventoryComponent } from '@routes/inventory/inventory.component';
+import { ReportsComponent } from '@routes/reports/reports.component';
+import { BillingComponent } from '@routes/billing/billing.component';
+import { ConfigurationComponent } from '@routes/configuration/configuration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    LoginComponent,
+    RegisterComponent,
+    LandingPageComponent,
+    PaymentComponent,
+    DashboardComponent,
+    IngresosComponent,
+    EgresosComponent,
+    InventoryComponent,
+    ReportsComponent,
+    BillingComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
