9e44c1a882bf31d2a4ba514a80c820f7a5323110
Francisco Caviglia <caviglia80@gmail.com>
2023-09-08 01:51:53 -0300

    .

--- src/app/shared/components/title/title.component.ts
+++ src/app/shared/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SharedService } from '@services/shared.service';
 
 @Component({
@@ -6,12 +6,17 @@ import { SharedService } from '@services/shared.service';
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
   @Input() public title: string = '';
+  public selectedItem: { key: string; value: any }[] | null = null;
 
   constructor(
     private sharedService: SharedService
   ) { }
+
+  ngOnInit() {
+    this.sharedService.getDataRow().subscribe((valor: any) => { this.selectedItem = valor; });
+  }
 
   public cambiarValor() {
     this.sharedService.setDataRow(null);
