15c9f9e0ff31b6f1a8e3fa95f91fcb75d51529e4
Francisco Caviglia <caviglia80@gmail.com>
2023-10-06 18:21:14 -0300

    .

--- src/app/shared/components/inventario-config/inventario-config.component.ts
+++ src/app/shared/components/inventario-config/inventario-config.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { DataService } from '@services/data/data.service';
+import { configuracion } from '@models/mainClasses/main-classes';
 
 @Component({
   selector: 'app-inventario-config',
   templateUrl: './inventario-config.component.html',
   styleUrls: ['./inventario-config.component.css']
 })
-export class InventarioConfigComponent {
+export class InventarioConfigComponent implements AfterViewInit {
+  public dataConfig: configuracion = new configuracion();
+  public pvp: string = '0';
+
+  constructor(
+    public dataService: DataService,
+  ) { }
+
+  ngAfterViewInit() {
+    this.dataInit();
+  }
+
+  private dataInit() {
+    this.dataService.Configuracion$.subscribe((data) => {
+      this.dataConfig = data[0];
+      if (this.dataConfig !== undefined) {
+        this.pvp = this.dataConfig.pvpPorcentaje;
+      }
+    });
+  }
+
+  public setPvp(pvp: string) {
+    if (pvp !== this.dataConfig.pvpPorcentaje)
+      this.dataService.fetchConfiguracion('PUT', { id: 1, pvpPorcentaje: pvp });
+  }
 
 }
