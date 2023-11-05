3ec835a2c0293f91150fc138a4f457756dcf9af2
caviglia80 <caviglia80@gmail.com>
2023-11-05 20:13:21 -0300

    .

--- src/app/routes/externo/landing-page/landing-page.component.ts
+++ src/app/routes/externo/landing-page/landing-page.component.ts
@@ -1,17 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
 
   constructor(private router: Router) { }
 
-  navigateToSidebar(str:string) {
-    this.router.navigate([str]);
+  ngAfterViewInit(): void {
+    // Navbar shrink function
+    const navbarShrink = () => {
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+        return;
+      }
+      if (window.scrollY === 0) {
+        navbarCollapsible.classList.remove('navbar-shrink');
+      } else {
+        navbarCollapsible.classList.add('navbar-shrink');
+      }
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+        target: '#mainNav',
+        rootMargin: '0px 0px -40%',
+      });
+    }
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = Array.from(document.querySelectorAll('#navbarResponsive .nav-link'));
+    responsiveNavItems.map((responsiveNavItem: Element) => {
+      responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler as Element).display !== 'none') {
+          (navbarToggler as HTMLElement).click();
+        }
+      });
+    });
   }
 
+
+  navigateToSidebar(str: string) {
+    this.router.navigate([str]);
+  }
 }
+
