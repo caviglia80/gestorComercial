b5261c84712f85bc069044efb98307ab472fd5dd
caviglia80 <caviglia80@gmail.com>
2023-11-14 11:34:14 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: 'register', loadChildren: () => import('@routes/Externo/register/register.module').then((m) => m.RegisterModule) },
   { path: 'payment', loadChildren: () => import('@routes/Externo/payment/payment.module').then((m) => m.PaymentModule) },
 
-  { path: 'nav', loadChildren: () => import('@routes/Interno/interno.module').then((m) => m.InternoModule) },
+  { path: 'nav', loadChildren: () => import('@app/routes/Interno/nav/nav.module').then((m) => m.InternoModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
