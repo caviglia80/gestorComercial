bfaf7014703e1633ce8b4d58a39725d1d18e09b1
caviglia80 <caviglia80@gmail.com>
2023-11-15 12:43:17 -0300

    .

--- src/app/routes/Interno/dashboard/dashboard.module.ts
+++ src/app/routes/Interno/dashboard/dashboard.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '@routes/Interno/dashboard/dashboard.component';
-import { DashboardGraficoIngresosComponent } from '@components/dashboard-grafico-ingresos/dashboard-grafico-ingresos.component';
-import { DashboardGraficoEgresosComponent } from '@components/dashboard-grafico-egresos/dashboard-grafico-egresos.component';
-import { DashboardGraficoMargenComponent } from '@components/dashboard-grafico-margen/dashboard-grafico-margen.component';
-import { DashboardGraficoMargenMenosEgresosComponent } from '@components/dashboard-grafico-margen-menos-egresos/dashboard-grafico-margen-menos-egresos.component';
-import { DashboardGraficoMediosdepagoComponent } from '@components/dashboard-grafico-mediosdepago/dashboard-grafico-mediosdepago.component';
+import { DashboardGraficoIngresosComponent } from './dashboard-grafico-ingresos/dashboard-grafico-ingresos.component';
+import { DashboardGraficoEgresosComponent } from './dashboard-grafico-egresos/dashboard-grafico-egresos.component';
+import { DashboardGraficoMargenComponent } from './dashboard-grafico-margen/dashboard-grafico-margen.component';
+import { DashboardGraficoMargenMenosEgresosComponent } from './dashboard-grafico-margen-menos-egresos/dashboard-grafico-margen-menos-egresos.component';
+import { DashboardGraficoMediosdepagoComponent } from './dashboard-grafico-mediosdepago/dashboard-grafico-mediosdepago.component';
 import { MaterialModule } from '@app/shared/modules/material/material.module';
 import { NgChartsModule } from 'ng2-charts';
 
