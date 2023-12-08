f32941df95d09bd791b8560badc5c2b7806b659f
caviglia80 <caviglia80@gmail.com>
2023-12-08 20:35:00 +0100

    .

--- src/app/routes/externo/register/register.component.ts
+++ src/app/routes/externo/register/register.component.ts
@@ -10,11 +10,14 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   public nombreCompleto: string = "";
-  public correo: string = "";
   public telefono: string = "";
-  public usuario: string = "";
+
+  public correo: string = "";
+  public confirmarCorreo: string = "";
+
   public clave: string = "";
   public confirmarClave: string = "";
+
   public errorMsg: string = '';
   public loading: boolean = false;
 
@@ -41,16 +44,40 @@ export class RegisterComponent implements OnInit {
       });
   }
 
+  isValidEmail(correo: string): boolean {
+    const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regexCorreo.test(correo);
+  }
+
+  onlyUser(correo: string): string {
+    return correo.split('@')[0];
+  }
+
   public Registro() {
+
+    if (this.correo !== this.confirmarCorreo) {
+      this.errorMsg = 'Ingreso mal el correo.';
+      return;
+    } else if (!this.isValidEmail(this.correo)) {
+      this.errorMsg = 'Ingreso un correo invalido.';
+      return;
+    } else if (this.clave !== this.confirmarClave) {
+      this.errorMsg = 'Ingreso mal la contraseña.';
+      return;
+    } else if (this.clave.length < 8) {
+      this.errorMsg = 'Debe ingresar una contraseña igual o mayor a 8 caracteres.';
+      return;
+    }
+
     this.errorMsg = '';
     this.loading = true;
 
     const body: User = {
       isNewAdmin: '1',
-      username: this.usuario,
-      fullname: this.nombreCompleto,
-      phone: this.telefono,
-      email: this.correo,
+      username: this.onlyUser(this.correo).trim(),
+      fullname: this.nombreCompleto.trim(),
+      phone: this.telefono.trim(),
+      email: this.correo.trim(),
       password: this.clave
     };
     this.dataService.fetchUsuarios('POST', body);
