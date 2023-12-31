3067867edcfc7c08f6f21e1771c50e2c03069597
caviglia80 <caviglia80@gmail.com>
2023-12-31 20:55:25 +0100

    .

--- src/php/DB/config.php
+++ src/php/DB/config.php
@@ -1,6 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../../storagedir/private/dbConfig.php';
-?>
 
-// Instalacion: tener en cuenta que las credenciales de la base de datos se encuentran en:
-// /storagedir/private/dbConfig.php ($servername,$username,$password,$dbname)
+#  Instalacion: tener en cuenta que las credenciales de la base de datos se encuentran en:
+#  /storagedir/private/dbConfig.php ($servername,$username,$password,$dbname)
+?>
